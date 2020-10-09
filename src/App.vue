66a4721f4ca5fe35f9ab807b169ff2767d5cfaf4
tony140407 <a32715368@gmail.com>
2020-10-09 18:57:50 +0800

    eslint problem

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="nav">
+  <div id="nav" class="container">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import '@/assets/all.scss';
+import '@/assets/scss/all.scss';
 
 createApp(App)
   .use(router)
